9063ec7f526b45b99a9e0c57e335f51eac8a4cfb
a.c <acskivko@gmail.com>
2025-12-29 12:02:33 +0200

    Add UserLogoutView, logout page, and logout button on profile page

--- my_project/user/urls.py
+++ my_project/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import UserLoginView, CreateUserView, UserProfileView
+from user.views import UserLoginView, CreateUserView, UserProfileView, UserLogoutView
 
 urlpatterns = [
     path('register/',CreateUserView.as_view(),name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/',UserProfileView.as_view(), name='profile'),
+    path('logout/',UserLogoutView.as_view(), name='logout'),
 
 ]
--- my_project/user/views.py
+++ my_project/user/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView, UpdateView, TemplateView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 # Create your views here.
@@ -12,6 +15,8 @@ class CreateUserView(CreateView):
     model = User
     form_class = UserCreationForm
     template_name = 'user/register_form.html'
+    success_url = reverse_lazy('user:login')
+
 
 
 class UserLoginView(LoginView):
@@ -21,3 +26,11 @@ class UserLoginView(LoginView):
 
 class UserProfileView(LoginRequiredMixin,TemplateView):
     template_name = 'user/profile.html'
+
+
+class UserLogoutView(LoginRequiredMixin,TemplateView):
+    template_name = 'user/logout.html'
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('login')
